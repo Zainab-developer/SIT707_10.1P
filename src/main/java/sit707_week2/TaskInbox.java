7759d23af08cfcf76edfe9bb58db7e3786deba1e
Zainab-developer <62815659+Zainab-developer@users.noreply.github.com>
2024-05-22 17:55:31 +1000

    Update TaskInbox.java

--- src/main/java/sit707_week2/TaskInbox.java
+++ src/main/java/sit707_week2/TaskInbox.java
@@ -7,9 +7,7 @@ import java.util.ArrayList;
 public class TaskInbox {
     private List<String> tasks;
 
-    public TaskInbox() {
-        tasks = new ArrayList<>();
-    }
+ 
 
     public List<String> getTasks() {
         return tasks;
